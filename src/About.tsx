efb2f62193a58f66fc73dc25dcd8b5d0febeccf1
Takayuki Miyauchi <miya0001@users.noreply.github.com>
2020-04-18 07:17:49 +0900

    some fix

--- src/About.tsx
+++ src/About.tsx
@@ -1,9 +1,23 @@
 import React from "react";
-
+import './About.scss'
+import bento from './bento.svg'
+import QRCode from 'qrcode.react'
 
 const Content = () => {
+  const url = window.location.href.replace(/#.+$/, '')
+
   return (
-    <div className="about">About</div>
+    <div className="about">
+      <div className="branding">
+        <div className="image"><img src={bento} alt=""/></div>
+        <div className="logo">イエメシ</div>
+      </div>
+
+      <h1>和歌山県串本町</h1>
+      <h2>テイクアウトマップ</h2>
+      <div className="qrcode"><QRCode value={url} bgColor="transparent" fgColor="#FFFFFF" /></div>
+      <p>ここに共有ボタン</p>
+    </div>
   );
 };
 
