84d89d1bb2b677e0b6bcf4ffbd5716109ff71fca
kamataryo <mugil.cephalus+Ciconia.local@gmail.com>
2020-04-17 20:23:09 +0900

    Use hashrouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { HashRouter as Router, Switch, Route, Link } from "react-router-dom";
 import logo from "./logo.svg";
 import "./App.css";
 
