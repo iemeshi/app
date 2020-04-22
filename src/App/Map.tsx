1fe55b0e4fa6796097c0872260bf7bd9367069cd
Takayuki Miyauchi <miya0001@users.noreply.github.com>
2020-04-22 14:46:29 +0900

    some fix

--- src/App/Map.tsx
+++ src/App/Map.tsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import data from '../data.json'
 import config from '../config.json'
-
+// @ts-ignore
+import geojsonExtent from '@mapbox/geojson-extent'
 import toGeoJson from './toGeoJson'
 
 type Props = {
@@ -13,9 +14,55 @@ const CSS: React.CSSProperties = {
   height: '100%',
 }
 
-const isDayTime = () => {
-  const hours = new Date().getHours()
-  return hours >= 5 && hours < 19
+const setCluster = (map: any) => {
+  map.addLayer({
+    id: 'clusters',
+    type: 'circle',
+    source: 'shops',
+    filter: ['has', 'point_count'],
+    paint: {
+      'circle-radius': 20,
+      'circle-color': '#FF0000',
+      'circle-opacity': 1.0,
+    },
+  })
+
+  map.addLayer({
+    id: 'cluster-count',
+    type: 'symbol',
+    source: 'shops',
+    filter: ['has', 'point_count'],
+    paint: {
+      'text-color': '#FFFFFF',
+    },
+    layout: {
+      'text-field': '{point_count_abbreviated}',
+      'text-size': 14,
+      'text-font': ['Noto Sans Regular'],
+    },
+  })
+
+  map.on('click', 'clusters', (e: any) => {
+    const features = map.queryRenderedFeatures(e.point, { layers: ['clusters'] })
+    const clusterId = features[0].properties.cluster_id
+    map.getSource('shops').getClusterExpansionZoom(clusterId, (err: any, zoom: number) => {
+      if (err)
+        return
+
+      map.easeTo({
+        center: features[0].geometry.coordinates,
+        zoom: zoom,
+      })
+    })
+  })
+
+  map.on('mouseenter', 'clusters', function () {
+    map.getCanvas().style.cursor = 'pointer'
+  })
+
+  map.on('mouseleave', 'clusters', function () {
+    map.getCanvas().style.cursor = ''
+  })
 }
 
 const Content = (props: Props) => {
@@ -26,15 +73,10 @@ const Content = (props: Props) => {
       return
     }
 
-    const geojson = toGeoJson(data)
-
     // @ts-ignore
     const { geolonia } = window;
 
-    let style = 'geolonia/basic'
-    if (!isDayTime()) {
-      style = 'geolonia/midnight'
-    }
+    const style = 'geolonia/basic'
 
     const container = document.createElement('div')
     container.dataset.geolocateControl = 'on'
@@ -54,15 +96,72 @@ const Content = (props: Props) => {
       style: style,
     });
 
-    document.addEventListener('visibilitychange', () => {
-      if ('visible' === document.visibilityState) {
-        if (isDayTime()) {
-          map.setStyle('geolonia/basic')
-        } else {
-          map.setStyle('geolonia/midnight')
-        }
-      }
+    const textColor = '#000000'
+    const textHaloColor = '#FFFFFF'
+
+    map.on('load', () => {
+      map.setLayoutProperty('building', 'visibility', 'none')
+      map.setLayoutProperty('poi', 'visibility', 'none')
+      map.setLayoutProperty('poi-primary', 'visibility', 'none')
+
+      const geojson = toGeoJson(data)
+      const bounds = geojsonExtent(geojson)
+      map.fitBounds(bounds, {
+        duration: 0,
+        padding: 100,
+      })
+
+      map.addSource('shops', {
+        type: 'geojson',
+        data: geojson,
+        cluster: true,
+        clusterMaxZoom: 14,
+        clusterRadius: 50,
+      })
+
+      map.addLayer({
+        id: 'shop-points',
+        type: 'circle',
+        source: 'shops',
+        filter: ['all',
+          ['==', '$type', 'Point'],
+        ],
+        paint: {
+          'circle-radius': 13,
+          'circle-color': '#FF0000',
+          'circle-opacity': 0.4,
+          'circle-stroke-width': 2,
+          'circle-stroke-color': '#FFFFFF',
+          'circle-stroke-opacity': 1,
+        },
+      })
+
+      map.addLayer({
+        id: 'shop-symbol',
+        type: 'symbol',
+        source: 'shops',
+        filter: ['all',
+          ['==', '$type', 'Point'],
+        ],
+        paint: {
+          'text-color': textColor,
+          'text-halo-color': textHaloColor,
+          'text-halo-width': 2,
+        },
+        layout: {
+          'text-field': "{店名}",
+          'text-font': ['Noto Sans Regular'],
+          'text-size': 12,
+          'text-anchor': 'top',
+          'text-max-width': 12,
+          'text-offset': [0, 1.2],
+          'text-allow-overlap': false,
+        },
+      })
+
+      setCluster(map)
     })
+
   }, [mapNode, props.orientation])
 
   return (
