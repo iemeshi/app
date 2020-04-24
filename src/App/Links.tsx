496ff7d5d2f27b35b6c675137821e8b43cd2f26e
kamataryo <mugil.cephalus+Ciconia.local@gmail.com>
2020-04-24 20:00:01 +0900

    Stop event propergation if not needed

--- src/App/Links.tsx
+++ src/App/Links.tsx
@@ -7,14 +7,18 @@ type Props = {
   data: Iemeshi.ShopData;
 };
 
+const noop = (e: React.MouseEvent) => {
+  e.stopPropagation()
+}
+
 const Content = (props: Props) => {
 
   return (
     <div className="links">
-      {props.data['電話番号']?<div className="link"><a href={`tel:${props.data['電話番号']}`}><FaPhone size="20px" /></a></div>:''}
-      {props.data['Instagram']?<div className="link"><a href={`https://instagram.com/${props.data['Instagram']}`}><FaInstagram size="20px" /></a></div>:''}
-      {props.data['Twitter']?<div className="link"><a href={`https://twitter.com/${props.data['Twitter']}`}><FaTwitter size="20px" /></a></div>:''}
-      {props.data['公式サイト']?<div className="link"><a href={props.data['公式サイト']}><FaHome size="20px" /></a></div>:''}
+      {props.data['電話番号']?<div className="link"><a href={`tel:${props.data['電話番号']}`}><FaPhone onClick={noop} size="20px" /></a></div>:''}
+      {props.data['Instagram']?<div className="link"><a href={`https://instagram.com/${props.data['Instagram']}`}><FaInstagram onClick={noop} size="20px" /></a></div>:''}
+      {props.data['Twitter']?<div className="link"><a href={`https://twitter.com/${props.data['Twitter']}`}><FaTwitter onClick={noop} size="20px" /></a></div>:''}
+      {props.data['公式サイト']?<div className="link"><a href={props.data['公式サイト']}><FaHome onClick={noop} size="20px" /></a></div>:''}
     </div>
   );
 };
