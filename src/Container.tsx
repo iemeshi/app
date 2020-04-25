8607c7eb24ae40146f10a64c30f13f82771e3871
Takayuki Miyauchi <miya0001@users.noreply.github.com>
2020-04-26 03:57:33 +0900

    some fix

--- src/Container.tsx
+++ src/Container.tsx
@@ -5,6 +5,13 @@ import About from './About'
 import './Container.scss'
 
 function Content() {
+  window.addEventListener('orientationchange', () => {
+    // We execute the same script as before
+    let vh = window.innerHeight * 0.01;
+    console.log(vh)
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  });
+
   return (
     <div className="outer-container">
       <div className="inner-container">
